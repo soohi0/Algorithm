1a190142e2ba4033965fd7482af5cb26c757b00a
soohi0 <soohi0966@naver.com>
2020-07-09 15:57:35 +0900

    priority_queue 이용

--- Algorithms/Algorithms/2751_수정렬하기.cpp
+++ Algorithms/Algorithms/2751_수정렬하기.cpp
@@ -1,7 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <queue>
 
 using namespace std;
 
 int main() {
+	int N; 
+	scanf("%d", &N);
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		scanf("%d", &num);
+		pq.push(num);
+	}
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", pq.top());
+		pq.pop();
+	}
 
 }
